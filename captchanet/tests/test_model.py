952ee6bd5a76368a9beb1dd4ee99f01edb1ee458
Hadrien Mary <hadrien.mary@gmail.com>
2019-07-01 15:48:30 -0400

    add tests

---
+++ captchanet/tests/test_model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+import captchanet
+
+def test_build_model():
+    image_shape = (400, 120, 3)
+    image_type = 'float32'
+    vocab_size = 50
+    max_len_word = 10
+    model = captchanet.build_model(image_shape, image_type, vocab_size, max_len_word)
+    assert model.count_params() == 10760764
