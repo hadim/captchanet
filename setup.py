b487dd5040a9d5b43f12c32a918ea950c6f9a938
Hadrien Mary <hadrien.mary@gmail.com>
2019-07-13 23:51:23 -0400

    bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='captchanet',
-      version='0.1.0',
+      version='0.2.0',
       author='Hadrien Mary',
       author_email='hadrien.mary@gmail.com',
       url='https://github.com/hadim/captchanet/',
